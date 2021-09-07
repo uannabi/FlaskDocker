dc1612c56098df69b797cbb25f6d1551efd42cff
uannabi <zahid_sc@hotmail.fr>
2021-09-07 19:31:23 +0600

    again modified

--- week4/resources/lab_helper.sh
+++ week4/resources/lab_helper.sh
@@ -10,15 +10,15 @@ sleep 2
 cd /Users/khoundokarzahid/Stack/github/FlaskDocker/week4/resources/price_api
 docker build --tag price-api .
 sleep 2
-aws ecr create-repository --repository-name service-api --region us-west-2 --query repository.repositoryUri
-aws ecr create-repository --repository-name price-api --region us-west-2 --query repository.repositoryUri
+aws ecr create-repository --repository-name service-api --region ap-southeast-1 --query repository.repositoryUri
+aws ecr create-repository --repository-name price-api --region ap-southeast-1 --query repository.repositoryUri
 #docker volume create my-named-shared-data
 
-service_api_image_uri=$(eval echo $(aws ecr describe-repositories --repository-name service-api --region us-west-2 --query repositories[0].repositoryUri))
-price_api_image_uri=$(eval echo $(aws ecr describe-repositories --repository-name price-api --region us-west-2 --query repositories[0].repositoryUri))
+service_api_image_uri=$(eval echo $(aws ecr describe-repositories --repository-name service-api --region ap-southeast-1 --query repositories[0].repositoryUri))
+price_api_image_uri=$(eval echo $(aws ecr describe-repositories --repository-name price-api --region ap-southeast-1 --query repositories[0].repositoryUri))
 #damn quotes ^
 sleep 2
-aws ecr get-login --region us-west-2 --no-include-email | /bin/bash
+aws ecr get-login --region ap-southeast-1 --no-include-email | /bin/bash
 sleep 2
 docker tag service-api $service_api_image_uri
 docker tag price-api $price_api_image_uri
@@ -34,10 +34,10 @@ sudo yum install -y zlib-devel openssl-devel ncurses-devel libffi-devel sqlite-d
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
 sleep 2
-ecs-cli configure --cluster fancy-cluster --default-launch-type EC2 --config-name fancy-cluster --region us-west-2
+ecs-cli configure --cluster fancy-cluster --default-launch-type EC2 --config-name fancy-cluster --region ap-southeast-1
 
 sleep 4
-ecs-cli up --keypair vockey --capability-iam --size 4 --instance-type m4.large --cluster-config fancy-cluster --port 8080 --region us-west-2
+ecs-cli up --keypair vockey --capability-iam --size 4 --instance-type m4.large --cluster-config fancy-cluster --port 8080 --region ap-southeast-1
 
 cd /home/ec2-user/environment/resources
 echo service_api_image_uri=$service_api_image_uri
